11eedb79eda890fa70ad23783d43175da72943c9
Kof <Kof@DESKTOP-VHFB8RK>
2017-12-05 22:47:44 +0200

    MERGE

--- DAL/Idal.cs
+++ DAL/Idal.cs
@@ -7,12 +7,8 @@ using System.Threading.Tasks;
 
 namespace DAL
 {
-  
     interface Idal
-
-    {
-       
-        /// <summary>
+    {       /// <summary>
             /// Nanny's Functions
             /// </summary>
             /// <param name="n"></param>
