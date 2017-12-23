e45809d8aeeaf55614f0d06bc7932f399989d3a0
Matanya Freiman <mfreiman97@gmail.com>
2017-12-23 18:43:13 +0200

    I fixed a critical error that i discovered in saturday (before the sleeping)

--- BL/ourBL_2.cs
+++ BL/ourBL_2.cs
@@ -32,12 +32,15 @@ namespace BL//MATANYA FUNCTIONS
         {
             for(int i=0;i<6;i++)
             {
+                if(m.needNanny[i]==true)
+                { 
                 for (int j = 0; j < 2; j++)
                 {
                     if (j==0 && 0 < n.schedule[j, i].CompareTo(m.timeWork[j, i]))//the starting time of the nanny should be earlier than of the mother
                         return false;
                     if (j == 1 && 0 > n.schedule[j, i].CompareTo(m.timeWork[j, i]))
                         return false;
+                }
                 }
             }
             return true;
