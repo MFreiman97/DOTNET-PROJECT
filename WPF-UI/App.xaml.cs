bce9e61351b76c4c482d04d635b1e0c6acabfa6a
Matanya Freiman <mfreiman97@gmail.com>
2017-12-25 19:01:41 +0200

    fwe

--- WPF-UI/App.xaml.cs
+++ WPF-UI/App.xaml.cs
@@ -13,5 +13,6 @@ namespace WPF_UI
     /// </summary>
     public partial class App : Application
     {
+       
     }
 }
--- WPF-UI/MainWindow.xaml.cs
+++ WPF-UI/MainWindow.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;//matanya
-using FirstFloor.ModernUI.Presentation;
-using FirstFloor.ModernUI.Windows.Controls;
+
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
