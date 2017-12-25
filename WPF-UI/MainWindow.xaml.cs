90dc8a363491e3ea2678835d6f49c8ab50367d74
Matanya Freiman <mfreiman97@gmail.com>
2017-12-25 18:11:32 +0200

    Adding the opputunity of mui

--- WPF-UI/MainWindow.xaml.cs
+++ WPF-UI/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;//matanya
+using FirstFloor.ModernUI.Presentation;
+using FirstFloor.ModernUI.Windows.Controls;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
