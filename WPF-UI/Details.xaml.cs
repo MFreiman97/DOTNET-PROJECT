6cb0e34e2980a21911bd1b1774b25ffcbc12214e
Matanya Freiman <mfreiman97@gmail.com>
2018-01-03 14:37:38 +0200

    I made a window that will show a details of what clicked. maybe there is an oppurtunity of updating the data.

---
+++ WPF-UI/Details.xaml.cs
@@ -0,0 +1,59 @@
+﻿using BE;
+using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_UI
+{
+    /// <summary>
+    /// Interaction logic for Details.xaml
+    /// </summary>
+    public partial class Details : MetroWindow
+    {
+        //Child c;
+        //Nanny n;
+        //Contract co;
+        //Mother m;
+        public Details(Object obj)
+        {
+            InitializeComponent();
+            if(obj is Child)
+            {
+                WhatToShowTB.Text = "Child";
+                Child c = obj as Child;
+                DetailsOfChild.DataContext = c;
+            }
+            if (obj is Mother)
+            {
+                WhatToShowTB.Text = "Mother";
+                Mother m = obj as Mother;
+             DetailsOfMother.DataContext = m;
+            }
+            if (obj is Nanny)
+            {
+                WhatToShowTB.Text = "Nanny";//using the trigger
+                Nanny n = obj as Nanny;
+              DetailsOfNanny.DataContext = n;
+            }
+            if (obj is Contract)
+            {
+                WhatToShowTB.Text = "Contract";//using the trigger
+                Contract co = obj as Contract;
+               DetailsOfContract.DataContext = co;
+            }
+        }
+
+       
+    }
+}
